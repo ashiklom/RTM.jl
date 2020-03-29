f11be20e0c308bae6ea55944f446a6296ad385e9
Alexey Shiklomanov <alexey.shiklomanov@gmail.com>
2020-03-29 17:38:16 -0400

    Implement generalized plate model (GPM)
    ...and add test.

--- compare-with-rrtm.R
+++ compare-with-rrtm.R
@@ -11,3 +11,25 @@ julia_source("./tav_abs.jl")
 tav_jl <- julia_call("tav_abs.", theta, refractive)
 
 expect_equal(tav_r, tav_jl)
+
+# Test GPM
+julia_eval("using GSL: sf_expint_E1")
+julia_source("./gpm.jl")
+
+kmat <- rrtm:::dataspec_p4
+N <- 1.4
+k <- kmat %*% c(40, 0.01, 0.01) / N
+gpm_r <- rrtm:::gpm(k, refractive, N)
+gpm_r_refl <- gpm_r[,,1]
+gpm_r_trans <- gpm_r[,,2]
+
+gpm_jl <- julia_call("gpm.", k, refractive, N)
+gpm_jl_refl <- numeric(2101)
+gpm_jl_trans <- numeric(2101)
+for (i in seq_len(2101)) {
+  gpm_jl_refl[i] <- gpm_jl[i][[1]]
+  gpm_jl_trans[i] <- gpm_jl[i][[2]]
+}
+
+expect_equal(gpm_jl_refl, gpm_r_refl)
+expect_equal(gpm_jl_trans, gpm_r_trans)
---
+++ gpm.jl
@@ -0,0 +1,47 @@
+function gpm(k, refractive, N)
+    if k <= 0.0
+        trans = 1.0
+    else
+        trans = (1 - k) * exp(-k) + k^2 * sf_expint_E1(k)
+    end
+
+    # Reflectance/transmittance of a single layer
+    talf = tav_abs(40, refractive)
+    ralf = 1 - talf
+    t12 = tav_abs(90, refractive)
+    r12 = 1 - t12
+    t21 = t12 / (refractive ^ 2)
+    r21 = 1 - t21
+
+    denom = 1 - r21 ^ 2 * trans ^ 2
+    Ta = talf * trans * t21 / denom
+    Ra = ralf + r21 * trans * Ta
+
+    t = t12 * trans * t21 / denom
+    r = r12 + r21 * trans * t
+
+    # Reflectance/transmittance of N layers
+    D = sqrt((1 + r + t) * (1 + r - t) * (1 - r + t) * (1 - r - t))
+    r2 = r ^ 2
+    t2 = t ^ 2
+    va = (1 + r2 - t2 + D) / (2 * r)
+    vb = (1 - r2 + t2 + D) / (2 * t)
+
+    vbNN = vb ^ (N - 1)
+    vbNN2 = vbNN ^ 2
+    va2 = va ^ 2
+    denomx = va2 * vbNN2 - 1
+    Rsub = va * (vbNN2 - 1) / denomx
+    Tsub = vbNN * (va2 - 1) / denomx
+
+    if r + t >= 1
+        Tsub = t / (t + (1 - t) * (N - 1))
+        Rsub = 1 - Tsub
+    end
+
+    denomy = 1 - Rsub * r
+    RN = Ra + Ta * Rsub * t / denomy
+    TN = Ta * Tsub / denomy
+   
+    RN, TN
+end
